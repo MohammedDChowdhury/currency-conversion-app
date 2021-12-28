23260c54532dcd65ed0dddd1a3958ca1a336996a
Mohammed <d2018c@gmail.com>
2021-12-28 17:29:17 +0000

    initial currency set

--- src/App.js
+++ src/App.js
@@ -5,22 +5,33 @@ import CurrencyRow from "./CurrencyRow";
 const BASE_URL = "https://api.exchangerate.host/latest";
 function App() {
   const [currencyOptions, currencySelector] = useState([]);
-  console.log(currencyOptions);
+  const [fromCurrency, setFromCurrency] = useState();
+  const [toCurrency, setToCurrency] = useState();
 
   useEffect(() => {
     fetch(BASE_URL)
       .then((res) => res.json())
       .then((data) => {
+        const initialCurrency = Object.keys(data.rates)[0];
         currencySelector([data.base, ...Object.keys(data.rates)]);
+        setFromCurrency(data.base);
+        setToCurrency(initialCurrency);
       });
   }, []);
   return (
     <>
       <h1>Convert</h1>
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={fromCurrency}
+      />
       <div className="equals">=</div>
 
-      <CurrencyRow currencyOptions={currencyOptions} />
+      <CurrencyRow
+        currencyOptions={currencyOptions}
+        selectedCurrency={toCurrency}
+        onChangeCurrency={(e) => setToCurrency(e.target.value)}
+      />
     </>
   );
 }
